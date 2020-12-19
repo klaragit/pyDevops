6c758ab73740d8dde7a370d17e5049e903ab8c13
Klara <mymobilek@gmail.com>
2020-12-19 12:31:25 +0200

    demo.py

---
+++ demo.py
@@ -0,0 +1,28 @@
+# shift + 3 => comment
+#  command + / => comment out all selected
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Chrome('/Users/k/Documents/DevOps/PythonForTesters/webdrivers/chromedriver')
+
+browser.get('https://www.google.com/doodles/')
+time.sleep(3)
+
+# scroller
+ # html = browser.find_element_by_tag_name('html')
+ # for i in range(10):
+ #    html.send_keys(Keys.DOWN)
+
+
+# xpathbutton = '//*[@id="hplogo"]/a/img'
+# button = browser.find_element_by_xpath(xpathbutton).click()
+
+
+
+# xpathsearchbox = '/html/body/div[2]/div[2]/form/div[2]/div[1]/div[1]/div/div[2]/input'
+# searcher = browser.find_element_by_xpath(xpathsearchbox).send_keys('markyshka')
+
+
+
+# browser.quit()
